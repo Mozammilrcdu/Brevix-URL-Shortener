356d396a38b0acc28158e8d504408df37322c9ca
MD MOZAMMIL ALAM <mdmozammilrcdu67@gmail.com>
2025-09-21 19:44:46 +0530

    Fixed redirect page

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,40 +1,42 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { BarLoader } from "react-spinners";
-import { getLongUrl } from "@/db/apiUrls";
-import { storeClicks } from "@/db/apiClicks";
+import {storeClicks} from "@/db/apiClicks";
+import {getLongUrl} from "@/db/apiUrls";
 import useFetch from "@/hooks/use-fetch";
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {BarLoader} from "react-spinners";
 
 const RedirectLink = () => {
-  const { id } = useParams();
-  const { loading, data, fn: fetchUrl } = useFetch(getLongUrl, id);
+  const {id} = useParams();
+
+  const {loading, data, fn} = useFetch(getLongUrl, id);
+
+  const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
 
   useEffect(() => {
-    fetchUrl();
+    fn();
   }, []);
 
   useEffect(() => {
-    if (!loading && data?.id && data?.original_url) {
-      let url = data.original_url;
-
-      if (!/^https?:\/\//i.test(url)) {
-        url = "https://" + url;
-      }
-
-      storeClicks({ id: data.id }).catch(console.error);
-
-      setTimeout(() => {
-        window.location.href = url;
-      }, 50); // 50ms delay
+    if (!loading && data) {
+      fnStats();
     }
-  }, [loading, data]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading]);
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <BarLoader width="80%" color="#36d7b7" />
-      <p className="mt-4 text-gray-700">Redirecting...</p>
-    </div>
-  );
+  if (loading || loadingStats) {
+    return (
+      <>
+        <BarLoader width={"100%"} color="#36d7b7" />
+        <br />
+        Redirecting...
+      </>
+    );
+  }
+
+  return null;
 };
 
 export default RedirectLink;
